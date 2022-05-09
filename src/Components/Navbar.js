302753cb794ad821ae83d3197a2a6503262544e0
Sagil7m <faraz15799@gmail.com>
2022-05-09 19:45:22 +0530

    new branch

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,1 +1,11 @@
-djla
+import React from 'react';
+import "../Css/Navbar.css";
+function Navbar() {
+  return (
+    <div className="sagil">
+      <h1>slkajfklf</h1>
+    </div>
+  )
+}
+
+export default Navbar
