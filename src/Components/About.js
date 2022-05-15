6ff3886b62ea28b7af9647bfc56008af1d031261
Mohsin <razam305@gmail.com>
2022-05-15 19:44:53 +0530

    Carousel

---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Styles/About.css'
+const About = () => {
+  return (
+   <div className="about">
+       <section>
+       <div className="about-image1"><img src="https://1tb.favim.com/preview/6/667/6671/66713/6671339.jpg" alt="" /></div>
+       <div className="about-content"><h1>Who are we?</h1><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore minus nobis sit aperiam fugiat reiciendis, inventore quos cupiditate error ducimus quae perspiciatis! Maxime quis quisquam perspiciatis, iure illo eaque placeat fugiat officiis praesentium fuga voluptate esse velit quos non quo.</p></div>
+       </section>
+       <section>
+       <div className="about-content"><h1>What we do...</h1><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore minus nobis sit aperiam fugiat reiciendis, inventore quos cupiditate error ducimus quae perspiciatis! Maxime quis quisquam perspiciatis, iure illo eaque placeat fugiat officiis praesentium fuga voluptate esse velit quos non quo.</p></div>
+      
+       <div className="about-image2"><img src="https://1tb.favim.com/preview/6/667/6671/66713/6671339.jpg" alt="" /></div> </section>
+   </div>
+  )
+}
+
+export default About
