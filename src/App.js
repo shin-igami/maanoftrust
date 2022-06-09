fb3eacfa3a7073ec64aa9db6971de865bd790fa3
Mohsin <razam305@gmail.com>
2022-06-09 22:25:35 +0530

    page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Topbar from './Components/Topbar';
 import Home from './Pages/Home/Home';
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import About from './Pages/About/About';
 import Team from './Pages/About/Team';
 import Mentor from './Pages/About/Mentor';
@@ -22,26 +22,26 @@ import CurrentProgram from './Pages/Home/CurrentProgram/CurrentProgram';
 function App() {
   return (
     <div className="App">
-      <Topbar/>
-<Navbar/>
+      <Topbar />
+      <Navbar />
 
-      
+
       <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="about-maanof" element={<About/> } />
-        <Route path="team" element={<Team/>} />
-        <Route path="mentors" element={<Mentor/>} />
-        <Route path="/impact/Diversity" element={ <Diversity/>} />
-        <Route path="/impact/creating-impact" element={ <CreateImpact/>} />
-        <Route path="/programs/focus-sectors" element={ <FocusSectors/>} />
-        <Route path="/programs/current-programs" element={ <CurrentProgram/>} />
-        <Route path="/contact/contact-us" element={<Contact/>} />
-        <Route path="/contact/jobs" element={ <Jobs/>} />
-        <Route path="/contact/become-a-mentor" element={ <BecomeMentor/>} />
-        <Route path="/contact/partnerwith-us" element={ <BeacomePartner/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about-maanof" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/mentors" element={<Mentor />} />
+        <Route path="/impact/Diversity" element={<Diversity />} />
+        <Route path="/impact/creating-impact" element={<CreateImpact />} />
+        <Route path="/programs/focus-sectors" element={<FocusSectors />} />
+        <Route path="/programs/current-programs" element={<CurrentProgram />} />
+        <Route path="/contact/contact-us" element={<Contact />} />
+        <Route path="/contact/jobs" element={<Jobs />} />
+        <Route path="/contact/become-a-mentor" element={<BecomeMentor />} />
+        <Route path="/contact/partnerwith-us" element={<BeacomePartner />} />
       </Routes>
-<Footer/>
- 
+      <Footer />
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ScrollToTop from './Components/ScrollTop';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <ScrollToTop/>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
