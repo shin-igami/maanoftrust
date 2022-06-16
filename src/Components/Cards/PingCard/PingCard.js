4983efd04add419c68b50ff467baf86c3db2ae23
Mohsin <razam305@gmail.com>
2022-06-16 17:13:50 +0530

    color change

--- src/Components/Cards/PingCard/PingCard.js
+++ src/Components/Cards/PingCard/PingCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./PingCard.css"
+import DemoPing from '../../../image/student.png'
 const PingCard = () => {
   return (
   <div className="PingCards">
@@ -7,7 +8,7 @@ const PingCard = () => {
       <div class="card">
       <div class="box">
         <div class="content">
-          <div className="img"><img src="" alt="" /></div>
+          <div className="img"><img src={DemoPing} alt="" /></div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
           <a href="#">Read More</a>
         </div>
