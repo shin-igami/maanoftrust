d1341b9b15341cb38b45c855d03f1cff31f11366
Mohsin <razam305@gmail.com>
2022-06-18 03:00:37 +0530

    website color finalise

--- src/Components/Cards/ContactCard/ContactCard.js
+++ src/Components/Cards/ContactCard/ContactCard.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import './ContactCard.css'
+import emailJs from 'emailjs-com'
+import { useNavigate } from 'react-router-dom'
 const ContactCard = () => {
+	const navigate = useNavigate();
+	const sendEmail =(e)=>{
+		e.preventDefault();
+		emailJs.sendForm('service_l018gtr','template_79gb27c',e.target,
+		'6u4r4hVtftEcMQbx2').then(res=>{
+			console.log(res);
+			alert("Submitted Successfully")
+			navigate('/');
+		}).catch(err=>{
+              console.log(err);
+		})
+	}
   return (
 
 <div class="ContactCard container">
@@ -27,18 +41,18 @@ const ContactCard = () => {
 			</div>
 			<div class="right-col">
 				<h2>Subscribe to Our Newsletter</h2>
-			<form>
+			<form onSubmit={sendEmail}>
 								<div class="result"></div>
 				<div class="form-group">
-					<input id="name" type="text" placeholder="enter your name"/>
+					<input id="name" name='name' type="text" placeholder="Enter your name"/>
 				</div>
 				
 				<div class="form-group">
-					<input id="email" type="email" placeholder="enter your email"/>
+					<input id="email" type="email" name='email' placeholder="Enter your email"/>
 				</div>
 				
 				<div class="form-group">
-					<input id="message" type="text" placeholder="enter your phone number"/>
+					<input id="message" type="text" name='phone' placeholder="Enter your phone number"/>
 				</div>
 				<button id="submit" type="submit">Submit</button>
 			</form>
