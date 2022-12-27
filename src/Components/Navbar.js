84a345a2a8f4795cb8feba798f2afe7db8b576e5
unknown <faraz15799@gmail.com>
2022-12-27 22:03:59 +0530

    job

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -67,7 +67,7 @@ const handleClick =() =>{
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Contact us
+                Contact US
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li><NavLink onClick={handleClick} class="dropdown-item" to="/contact/contact-us">Contact Us</NavLink></li>
