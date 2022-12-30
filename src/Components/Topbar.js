b2275ee719fdf0b7630a9e442eec269295d68b5c
unknown <faraz15799@gmail.com>
2022-12-30 11:56:29 +0530

    git removed from top bar

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -7,33 +7,19 @@ function Topbar() {
   return (
     <>
     <div className="topbar"> 
-git
    <div className="email">
-    
    <i className="fa-solid fa-envelope"></i>maanoffederation@gmail.com   
-    
    </div>
    <div className="phone">
-     
-
-   <i className="fa-solid fa-phone"></i>    9599380707
-
-    
+   <i className="fa-solid fa-phone"></i>9599380707
    </div>
-
    <div className="socialmedia">
      <a href="https://twitter.com/MAANOF22" target="_blank"><i className="fa-brands fa-twitter" id="twiter"></i></a>
      <a href="https://www.facebook.com/profile.php?id=100088649441827"  target="_blank"><i className="fa-brands fa-facebook-f" id="facebook"></i></a>
      <a href="https://www.instagram.com/maanoffederation/"  target="_blank" ><i className="fa-brands fa-instagram" id="insta"></i></a>
      <a href="https://www.youtube.com/channel/UC25RXBGG9Qp22sLnO6jS5ug"  target="_blank" ><i className="fa-brands fa-youtube" id="youtube"></i></a>
      <a href="https://www.linkedin.com/company/maanof-federation/?viewAsMember=true"  target="_blank"><i className="fa-brands fa-linkedin-in" id="linkdin"></i></a>
-     
-
-   
    </div>
-   
-
-
     </div>
     </>
   )
